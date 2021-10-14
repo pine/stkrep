cf1dcf7f2d14eff03985d155d19c1efacf2e0c88
Pine Mizune <pinemz@gmail.com>
2021-10-14 16:19:35 +0900

    chore: improve initial files

---
+++ app/src/test/java/moe/pine/stkrep/AppTest.java
@@ -0,0 +1,19 @@
+package moe.pine.stkrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AppTest {
+    @Autowired
+    private App app;
+
+    @Test
+    public void mainTest() {
+        assertNotNull(app);
+    }
+}
