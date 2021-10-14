8c383cc04d3e0e982e0c0b59dd4811ee841ecc07
Pine Mizune <pinemz@gmail.com>
2021-10-14 17:24:55 +0900

    chore: add health controller

---
+++ app/src/main/java/moe/pine/stkrep/controllers/package-info.java
@@ -0,0 +1,6 @@
+@NonNullApi
+@NonNullFields
+package moe.pine.stkrep.controllers;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
---
+++ app/src/main/java/moe/pine/stkrep/package-info.java
@@ -0,0 +1,7 @@
+@NonNullApi
+@NonNullFields
+package moe.pine.stkrep;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
+
