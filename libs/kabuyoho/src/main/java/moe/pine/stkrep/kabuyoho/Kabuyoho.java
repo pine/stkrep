8d39af176763f9fb768cee2a3755517f4632f4f3
Pine Mizune <pinemz@gmail.com>
2021-11-09 21:15:48 +0900

    refactor: add error log

--- libs/kabuyoho/src/main/java/moe/pine/stkrep/kabuyoho/Kabuyoho.java
+++ libs/kabuyoho/src/main/java/moe/pine/stkrep/kabuyoho/Kabuyoho.java
@@ -3,6 +3,8 @@ package moe.pine.stkrep.kabuyoho;
 import lombok.extern.slf4j.Slf4j;
 import moe.pine.stkrep.kabuyoho.browser.Browser;
 import moe.pine.stkrep.kabuyoho.browser.BrowsingResults;
+import moe.pine.stkrep.kabuyoho.exception.NonRetryableException;
+import moe.pine.stkrep.kabuyoho.exception.RetryableException;
 import moe.pine.stkrep.kabuyoho.extractor.Extractors;
 import moe.pine.stkrep.report.Forecast;
 
@@ -12,11 +14,17 @@ public class Kabuyoho {
     private static final Extractors EXTRACTORS = new Extractors();
 
     public Forecast find(int code) {
-        final BrowsingResults browsingResults = BROWSER.browse(code);
-        final Forecast forecast = EXTRACTORS.extract(browsingResults);
+        try {
+            final BrowsingResults browsingResults = BROWSER.browse(code);
+            final Forecast forecast = EXTRACTORS.extract(browsingResults);
 
-        log.debug("Finished getting the report: {}", forecast);
+            log.debug("Finished getting the report: {}", forecast);
 
-        return forecast;
+            return forecast;
+        } catch (RetryableException e) {
+            throw new RetryableException(String.format("Failed to find the report. [code=%d]", code), e);
+        } catch (RuntimeException e) {
+            throw new NonRetryableException(String.format("Failed to find the report. [code=%d]", code), e);
+        }
     }
 }
