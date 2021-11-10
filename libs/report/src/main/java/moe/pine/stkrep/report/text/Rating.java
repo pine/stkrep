6b54bdc3bb131fa4688026dd7fc5546de775de4e
Pine Mizune <pinemz@gmail.com>
2021-11-10 17:11:34 +0900

    refactor: add `NA`

--- libs/report/src/main/java/moe/pine/stkrep/report/text/Rating.java
+++ libs/report/src/main/java/moe/pine/stkrep/report/text/Rating.java
@@ -15,9 +15,10 @@ import java.util.Set;
 public enum Rating implements TextEnum {
     STRONG_BUY(Set.of("強気"), "強気", ForegroundColor.RED),
     MODERATE_BUY(Set.of("やや強気"), "やや強気", ForegroundColor.RED),
-    NEUTRAL(Set.of("中立", ""), "中立", ForegroundColor.BLACK),
+    NEUTRAL(Set.of("中立"), "中立", ForegroundColor.BLACK),
     MODERATE_SELL(Set.of("やや弱気"), "やや弱気", ForegroundColor.GREEN),
     STRONG_SELL(Set.of("弱気"), "弱気", ForegroundColor.GREEN),
+    NA(Set.of("--", ""), "", ForegroundColor.BLACK),
     ;
 
     private final Set<String> inputTexts;
