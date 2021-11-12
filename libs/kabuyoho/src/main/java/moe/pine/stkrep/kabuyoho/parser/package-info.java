8656810e02f660cbc98434e08cecaf203c3fe569
Pine Mizune <pinemz@gmail.com>
2021-11-12 10:45:57 +0900

    refactor: cosmetic change

---
+++ libs/kabuyoho/src/main/java/moe/pine/stkrep/kabuyoho/parser/package-info.java
@@ -0,0 +1,6 @@
+@NonNullApi
+@NonNullFields
+package moe.pine.stkrep.kabuyoho.parser;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
--- libs/sheets/src/main/java/moe/pine/stkrep/sheets/internal/LevelTexts.java
+++
@@ -1,9 +0,0 @@
-package moe.pine.stkrep.sheets.internal;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class LevelTexts {
-    public static final String HIGH = "高値圏警戒";
-    public static final String LOW = "底値圏突入";
-}
