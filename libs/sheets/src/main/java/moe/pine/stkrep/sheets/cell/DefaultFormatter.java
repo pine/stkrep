28330219e495c53f30dce03b9426a3cf8570e97c
Pine Mizune <pinemz@gmail.com>
2021-11-18 10:14:40 +0900

    fix: set background color

--- libs/sheets/src/main/java/moe/pine/stkrep/sheets/cell/DefaultFormatter.java
+++ libs/sheets/src/main/java/moe/pine/stkrep/sheets/cell/DefaultFormatter.java
@@ -1,12 +1,17 @@
 package moe.pine.stkrep.sheets.cell;
 
 import moe.pine.stkrep.report.Report;
+import moe.pine.stkrep.report.color.BackgroundColor;
 import moe.pine.stkrep.report.color.ForegroundColor;
 
 public class DefaultFormatter<R extends Report> implements Formatter<R> {
     @Override
     public void run(R report, FormatBuilder builder) {
-        builder.textForegroundColor(ForegroundColor.BLACK)
-                .textBold(report.isHighlighted());
+        builder.textForegroundColor(ForegroundColor.BLACK);
+        
+        if (report.isHighlighted()) {
+            builder.backgroundColor(BackgroundColor.YELLOW)
+                    .textBold(true);
+        }
     }
 }
