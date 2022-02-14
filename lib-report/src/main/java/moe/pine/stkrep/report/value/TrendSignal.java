07dfdf2a0bdf9e183329140d28883d23c016f1ab
Pine Mizune <pinemz@gmail.com>
2022-02-14 18:04:09 +0900

    fix: fix TrendSignal definition

--- lib-report/src/main/java/moe/pine/stkrep/report/value/TrendSignal.java
+++ lib-report/src/main/java/moe/pine/stkrep/report/value/TrendSignal.java
@@ -17,9 +17,10 @@ import java.util.Set;
 public enum TrendSignal implements TextEnum {
     BUY_CHANGED(Set.of("買い転換"), "買い転換", ForegroundColor.RED),
     CONTINUE_BUY(Set.of("買い継続"), "買い継続", ForegroundColor.RED),
-    NEUTRAL(Set.of("ニュートラル", ""), "ニュートラル", ForegroundColor.BLACK),
+    NEUTRAL(Set.of("ニュートラル"), "ニュートラル", ForegroundColor.BLACK),
     CONTINUE_SELL(Set.of("売り継続"), "売り継続", ForegroundColor.GREEN),
     SELL_CHANGED(Set.of("売り転換"), "売り転換", ForegroundColor.GREEN),
+    NA(Set.of("--", ""), "", ForegroundColor.BLACK),
     ;
 
     public static final Set<TrendSignal> BUY = Set.of(BUY_CHANGED, CONTINUE_BUY);
